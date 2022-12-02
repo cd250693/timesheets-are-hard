5df503096764401350d34fc9de854722431dab65
Chris Dawes <chris@hatchd.com.au>
2022-12-03 01:35:17 +0800

    add wordlist and util function to generate random words

--- src/utils.ts
+++ src/utils.ts
@@ -8,4 +8,14 @@ function deepCopyArray<Type>(value: Type[]) {
     return copy;
 }
 
-export { deepCopyArray };
+import { wordList } from './wordlist';
+
+function getRandomWords(numWords: number): string[] {
+    const words = [];
+    for (let i = 0; i < numWords; i++) {
+        words.push(wordList[Math.floor(Math.random() * wordList.length)]);
+    }
+    return words;
+}
+
+export { deepCopyArray, getRandomWords };
